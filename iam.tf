82b172c385dd013ac1e6178aa1453c7f7995714c
Hendrix Roa <hendrix.alex87@gmail.com>
2020-11-08 13:40:10 -0500

    Remove unused role

--- iam.tf
+++ iam.tf
@@ -349,24 +349,3 @@ resource "aws_iam_role_policy" "kibana_sns_policy" {
   role   = aws_iam_role.kibana_sns_role.id
   policy = data.aws_iam_policy_document.kibana_sns_policy.json
 }
-
-//VPC Role for flow logs
-resource "aws_iam_role" "vpc_flow_logs" {
-  name = "vpc_flow_logs_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "vpc-flow-logs.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
