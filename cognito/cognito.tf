1b326429a4eefb522101e9a8c19d528d7aa4adf5
Hendrix Roa <hendrix.alex87@gmail.com>
2021-02-12 20:49:58 -0500

    Update outputs

--- cognito/cognito.tf
+++ cognito/cognito.tf
@@ -46,13 +46,3 @@ resource "aws_cognito_identity_pool" "identity_pool" {
 
   lifecycle {ignore_changes = [cognito_identity_providers]}
 }
-
-
-output "cognito_map" {
-  description = "cognito info"
-  value       = { "user_pool"        = aws_cognito_user_pool.user_pool.id
-    "identity_pool"    = aws_cognito_identity_pool.identity_pool.id
-    "auth_arn"         = aws_iam_role.authenticated.arn
-    "domain"           = "${aws_cognito_user_pool_domain.user_pool_domain.domain}.auth.${var.region}.amazoncognito.com"
-  }
-}
--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "elasticsearch_arn" {
 output "kibana_sns_role" {
   value = aws_iam_role.kibana_sns_role.arn
 }
+
+output "cognito_user_pool_id" {
+  value = module.cognito.user_pool_id
+}
+
+output "cognito_identity_pool_id" {
+  value =module.cognito.identity_pool_id
+}
+
+output "cognito_app_client_id" {
+  value = module.cognito.app_client_id
+}
